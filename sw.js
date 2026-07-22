a3b657c539b5e0cdc13deafb67d4e2e10a849d25
primekozmetik <ozonrusya@gmail.com>
2026-07-22 15:32:57 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,2 @@
-const CACHE='prime-vip-v3';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(self.registration.unregister()));
